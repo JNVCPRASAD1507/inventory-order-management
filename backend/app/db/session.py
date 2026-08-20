66a294fc28e23850b2737f73308ff8a5db32ef78
JNVCPRASAD1507 <chandraprasadkolli@gmail.com>
2026-08-20 11:28:57 +0530

    changed postgres to sqlite in session.py file

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.core.config import settings
 
 connect_args = {}
-if settings.DATABASE_URL.startswith("postgresql"):
+if settings.DATABASE_URL.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
 engine = create_engine(
